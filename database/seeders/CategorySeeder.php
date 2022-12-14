3cdb3b65a60ed3f097c68230e8538c0043944fcb
phungvanphib1 <phungvanphib1@gmail.com>
2022-12-14 17:28:12 +0700

    editname-CategorySeeder

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class categorySeeder extends Seeder
+class CategorySeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -22,7 +22,7 @@ class categorySeeder extends Seeder
             ],
             [
                 'name' => 'SamSung',
-                
+
             ],
 
         ]);
