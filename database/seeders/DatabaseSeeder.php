d188532a3686c9b762620cadddf6e02510673d60
mxt <thaomx1995@gmail.com>
2022-12-14 19:32:53 +0700

    product-migration-seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,5 +23,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CategorySeeder::class,
         ]);
+        $this->call([
+            ProductSeeder::class,
+        ]);
     }
 }
