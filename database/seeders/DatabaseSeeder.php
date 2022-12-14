d5553a9b3647977720f6b29239a81d605b21a648
mxt <thaomx1995@gmail.com>
2022-12-14 21:20:21 +0700

    Order-Detail-migration-seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,6 +25,7 @@ class DatabaseSeeder extends Seeder
             CustomerSeeder::class,
             ProductSeeder::class,
             OderSeeder::class,
+            Oder_detailSeeder::class,
         ]);
 
     }
---
+++ database/seeders/Oder_detailSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class Oder_detailSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('order_detail')->insert([
+            [
+                'id' =>1,
+                'product_id' => 1,
+                'order_id' => 1,
+                'quantity' => 1,
+                'price' => 450000,
+            ],
+            [
+                'id' =>2,
+                'product_id' => 2,
+                'order_id' => 2,
+                'quantity' => 1,
+                'price' => 500000,
+            ],
+        ]);
+    }
+}
